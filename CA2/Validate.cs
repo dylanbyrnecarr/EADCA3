7f5fbcccd4bb6c544a5c5579b6ac88723b8fd3c9
dylanbyrnecarr <x00155165@mytudublin.ie>
2021-12-14 11:21:22 +0000

    uploading code

---
+++ CA2/Validate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidateSearch
+{
+    public class ValidSearch
+    {
+        [Required(ErrorMessage = "Search cannot be empty")]
+        [StringLength(30, ErrorMessage = "Search query too large, enter less than 2,000 characters.")]
+
+        public string SearchInput { get; set; }
+
+        public override string ToString()
+        {
+            return $"{SearchInput}";
+        }
+    }
+}
